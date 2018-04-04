ee5a47b2c5b86c6fb64af0d5073dde2f7a96ab72
Pedro Arias Perez <parias@delta12.aulas.gsyc.urjc.es>
2018-04-04 12:46:05 +0200

    Implementado views basicos

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hola(request):
+    html = '<html><body><h1>Hello world!</h1></body></html>'
+
+    return HttpResponse(html)
