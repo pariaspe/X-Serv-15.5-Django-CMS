19ab761532c57a72714d7461f2bec36ac6cd57d0
Pedro Arias Perez <parias@delta12.aulas.gsyc.urjc.es>
2018-04-04 12:46:24 +0200

    Implementado urls basicas

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'$^', 'cms.views.hola'),
     url(r'^admin/', include(admin.site.urls)),
 )
