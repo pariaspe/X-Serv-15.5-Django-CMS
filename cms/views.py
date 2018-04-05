b4b10c12e0c4999817bd4b00f87a79a02c356945
Pedro Arias Perez <parias@gamma06.aulas.gsyc.urjc.es>
2018-04-05 11:42:48 +0200

    Primera version para los metodos barra y pagina

--- cms/views.py
+++ cms/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def hola(request):
-    html = '<html><body><h1>Hello world!</h1></body></html>'
+from .models import Pages
 
+def barra(request):
+    lista = Pages.objects.all()
+    resp = '<ul>'
+    for page in lista:
+        resp += '<li>' + str(page.id) + '. ' + page.name
+    resp += '</ul>'
+    html = '<html><body><h1>Paginas existentes:</h1>' + resp + '</body></html>'
     return HttpResponse(html)
+
+def pagina(request, path):
+    try:
+        pagina = Pages.objects.get(name=path)
+    except Pages.DoesNotExist:
+            return HttpResponse("No existe")
+    return HttpResponse(pagina.page)
